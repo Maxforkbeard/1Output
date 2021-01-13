bc2722de876585f60a405b83048316f4ee983d81
Simon Counter <simonpcounter@gmail.com>
2021-01-13 11:28:56 +0000

    Investigate 4

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ word3 = "The"
 word4 = "Or"
 word5 = "And"
 
-print()
-print()
-print()
-print()
-print()
-#It runs blank lines
+print(word1 +" be, or not ot be: that is the question:\n")
+print(word2 +" 'tis nobler in the mind of suffer\n")
+print(word3 +" slings and arrows of outrageous fortune,\n")
+print(word4 +" to take arms against a sea of troubles,\n")
+print(word5 +" by opposing end them?\n")
+#It leaves a line between each print statement
