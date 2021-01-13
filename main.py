dd20c91327bbca496f8f637c6d8cebbc4368fc8b
Simon Counter <simonpcounter@gmail.com>
2021-01-13 11:16:52 +0000

    Run

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ print(word2 +" 'tis nobler in the mind of suffer")
 print(word3 +" slings and arrows of outrageous fortune,")
 print(word4 +" to take arms against a sea of troubles,")
 print(word5 +" by opposing end them?")
+#I was acurate
