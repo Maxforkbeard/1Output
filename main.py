81b633e3c4b856523be4ed969e05209ffce27e8f
Simon Counter <simonpcounter@gmail.com>
2021-01-13 11:22:21 +0000

    Investigate 2

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ word3 = "The"
 word4 = "Or"
 word5 = "And"
 
-print word1 +" be, or not ot be: that is the question:"
-print word2 +" 'tis nobler in the mind of suffer"
-print word3 +" slings and arrows of outrageous fortune,"
-print word4 +" to take arms against a sea of troubles,"
-print word5 +" by opposing end them?"
-#It errors and asks for the parenthses
+print(word1 + be, or not ot be: that is the question:)
+print(word2 + 'tis nobler in the mind of suffer)
+print(word3 + slings and arrows of outrageous fortune,)
+print(word4 + to take arms against a sea of troubles,)
+print(word5 + by opposing end them?)
+#It takes certain words as opperators
