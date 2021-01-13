5146caec8d8c8c4a2d4d17aa1ecb25c4e8b27e1a
Simon Counter <simonpcounter@gmail.com>
2021-01-13 11:32:54 +0000

    Investigate 5

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ word3 = "The"
 word4 = "Or"
 word5 = "And"
 
-print(word1 +" be, or not ot be: that is the question:\n")
-print(word2 +" 'tis nobler in the mind of suffer\n")
-print(word3 +" slings and arrows of outrageous fortune,\n")
-print(word4 +" to take arms against a sea of troubles,\n")
-print(word5 +" by opposing end them?\n")
-#It leaves a line between each print statement
+print("Line 1:" + word1 +" be, or not ot be: that is the question:")
+print("Line 2:" + word2 +" 'tis nobler in the mind of suffer")
+print("Line 3:" + word3 +" slings and arrows of outrageous fortune,")
+print("Line 4:" + word4 +" to take arms against a sea of troubles,")
+print("Line 5:" + word5 +" by opposing end them?")
+#It puts the Line x before the rest of each sentece, it still does it as Run
