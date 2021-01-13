4932ee382e1e57e5cc1e4fd8c9bad4f1232763cb
Simon Counter <simonpcounter@gmail.com>
2021-01-13 11:16:01 +0000

    OUTPUT Predict

---
+++ main.py
@@ -0,0 +1,12 @@
+# I predict that it will print the words and then the rest of the sentences respectively
+word1 = "To"
+word2 = "Whether"
+word3 = "The"
+word4 = "Or"
+word5 = "And"
+
+print(word1 +" be, or not ot be: that is the question:")
+print(word2 +" 'tis nobler in the mind of suffer")
+print(word3 +" slings and arrows of outrageous fortune,")
+print(word4 +" to take arms against a sea of troubles,")
+print(word5 +" by opposing end them?")
