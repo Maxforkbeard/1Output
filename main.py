2b85ce3da89e98ef1b6ee8814e16f0f17ab018b8
Simon Counter <simonpcounter@gmail.com>
2021-01-19 12:14:14 +0000

    Modify 3

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print("be, or not ot be: that is the question:")
-print("'tis nobler in the mind of suffer")
-print("slings and arrows of outrageous fortune,")
-print("to take arms against a sea of troubles,")
-print("by opposing end them?")
+word1 = "To"
+word2 = "Whether"
+word3 = "The"
+word4 = "Or"
+word5 = "And"
+
+print("be, or not ot be: that is the question: " + word1)
+print("'tis nobler in the mind of suffer " + word2)
+print("slings and arrows of outrageous fortune, " + word3)
+print("to take arms against a sea of troubles, " + word4)
+print("by opposing end them? " + word5)
