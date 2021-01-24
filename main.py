0a76de0cb55b1d9018aa3941bf9c95ef1edddb87
Simon Counter <simonpcounter@gmail.com>
2021-01-24 09:06:22 +0000

    Make 2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-word1 = "Frankfert"
-word2 = "Quaplla"
-word3 = "It"
-word4 = "How"
-word5 = "Maybey"
+word1 = "To"
+word2 = "Whether"
+word3 = "The"
+word4 = "Or"
+word5 = "And"
 
-print(word1 +" be, or not ot be: that is the question:")
-print(word2 +" 'tis nobler in the mind of suffer")
-print(word3 +" slings and arrows of outrageous fortune,")
-print(word4 +" to take arms against a sea of troubles,")
+print(word1 +" be, or \n not to be: \n that is the \n question:")
+print(word2 +" 'tis nobler in \n the mind of suffer")
+print(word3 +" slings and arrows of \n outrageous fortune,")
+print(word4 +" to take arms against a \n sea of troubles,")
 print(word5 +" by opposing end them?")
