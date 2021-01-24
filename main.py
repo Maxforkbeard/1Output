2a7281024b33afdbe17db4f006a6befb6874f88f
Simon Counter <simonpcounter@gmail.com>
2021-01-24 09:10:01 +0000

    Make 3

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-word1 = "To"
-word2 = "Whether"
-word3 = "The"
-word4 = "Or"
-word5 = "And"
-
-print(word1 +" be, or \n not to be: \n that is the \n question:")
-print(word2 +" 'tis nobler in \n the mind of suffer")
-print(word3 +" slings and arrows of \n outrageous fortune,")
-print(word4 +" to take arms against a \n sea of troubles,")
-print(word5 +" by opposing end them?")
+print("*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n**********")
